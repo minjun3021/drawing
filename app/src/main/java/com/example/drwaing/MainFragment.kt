505c03bbd8f603bb7ef9a1c0f664d454a4780aea
donggeun <nono1507@naver.com>
2022-03-13 15:16:16 +0900

    ViewBinding 익스텐션 추가

--- app/src/main/java/com/example/drwaing/MainFragment.kt
+++ app/src/main/java/com/example/drwaing/MainFragment.kt
@@ -1,32 +1,16 @@
 package com.example.drwaing
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.drwaing.databinding.FragmentMainBinding
+import com.example.drwaing.extension.viewBinding
 
 
 class MainFragment : Fragment() {
-    private var mBinding : FragmentMainBinding?=null
-    private val binding get()=mBinding!!
+    private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mBinding = FragmentMainBinding.inflate(inflater, container, false)
-
-        return binding.root
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mBinding=null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
